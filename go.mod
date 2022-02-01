7ef46b48c375d7c212633226c7bec2405ff8801d
Sergey Vilgelm <sergey@vilgelm.com>
2022-02-01 11:32:48 -0800

    tests

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/SVilgelm/yaml-benchmarks
+
+go 1.17
+
+require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/goccy/go-yaml v1.9.5 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ yaml_test.go
@@ -0,0 +1,185 @@
+package yaml_benchmarks
+
+import (
+	_ "embed"
+	"encoding/json"
+	"testing"
+
+	ghodss "github.com/ghodss/yaml"
+	goccy "github.com/goccy/go-yaml"
+	"github.com/stretchr/testify/require"
+	yamlV2 "gopkg.in/yaml.v2"
+	yamlV3 "gopkg.in/yaml.v3"
+)
+
+var (
+	//go:embed testdata/test.yaml
+	yamlFile []byte
+	//go:embed testdata/test.json
+	jsonFile []byte
+	jsonObj  interface{}
+)
+
+func init() {
+	if err := json.Unmarshal(jsonFile, &jsonObj); err != nil {
+		panic(err)
+	}
+}
+
+func yamlJSON(input interface{}) (interface{}, error) {
+	switch x := input.(type) {
+	case map[interface{}]interface{}:
+		m := make(map[string]interface{}, len(x))
+		nonstrings := make(map[interface{}]interface{})
+
+		// Store string keys and values as such.
+		for k, v := range x {
+			jv, err := yamlJSON(v)
+			if err != nil {
+				return nil, err
+			}
+
+			if s, ok := k.(string); ok {
+				m[s] = jv
+			} else {
+				nonstrings[k] = jv
+			}
+		}
+
+		// Convert non-string keys to JSON and then to strings, if possible, while giving preference to the string keys.
+		for k, jv := range nonstrings {
+			jk, err := yamlJSON(k)
+			if err != nil {
+				return nil, err
+			}
+
+			if raw, err := json.Marshal(jk); err != nil {
+				return nil, err
+			} else {
+				sk := string(raw)
+				if _, ok := m[sk]; !ok {
+					m[sk] = jv
+				}
+			}
+		}
+
+		return m, nil
+
+	case map[string]interface{}:
+		m := make(map[string]interface{}, len(x))
+
+		// Check sub items
+		for k, v := range x {
+			jv, err := yamlJSON(v)
+			if err != nil {
+				return nil, err
+			}
+
+			m[k] = jv
+		}
+
+		return m, nil
+
+	case []interface{}:
+		for i, v := range x {
+			if o, err := yamlJSON(v); err != nil {
+				return nil, err
+			} else {
+				x[i] = o
+			}
+		}
+	}
+
+	return input, nil
+}
+
+func compareWithJSON(t testing.TB, v interface{}) {
+	data, err := json.Marshal(v)
+	require.NoError(t, err)
+	require.JSONEq(t, string(jsonFile), string(data))
+}
+
+func TestUnmarshalToInterface(t *testing.T) {
+	t.Run("json", func(t *testing.T) {
+		var v interface{}
+		require.NoError(t, json.Unmarshal(jsonFile, &v))
+		compareWithJSON(t, v)
+	})
+	t.Run("yaml.v3", func(t *testing.T) {
+		var v interface{}
+		require.NoError(t, yamlV3.Unmarshal(yamlFile, &v))
+		j, err := yamlJSON(v)
+		require.NoError(t, err)
+		compareWithJSON(t, j)
+	})
+	t.Run("yaml.v2", func(t *testing.T) {
+		var v interface{}
+		require.NoError(t, yamlV2.Unmarshal(yamlFile, &v))
+		j, err := yamlJSON(v)
+		require.NoError(t, err)
+		compareWithJSON(t, j)
+	})
+	t.Run("ghodss", func(t *testing.T) {
+		var v interface{}
+		require.NoError(t, ghodss.Unmarshal(yamlFile, &v))
+		compareWithJSON(t, v)
+	})
+	t.Run("goccy", func(t *testing.T) {
+		var v interface{}
+		require.NoError(t, goccy.Unmarshal(yamlFile, &v))
+		compareWithJSON(t, v)
+	})
+}
+
+var benchmarkRes interface{}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	b.Run("json", func(b *testing.B) {
+		var res interface{}
+		for i := 0; i < b.N; i++ {
+			var v interface{}
+			_ = json.Unmarshal(jsonFile, &v)
+			res = v
+		}
+		benchmarkRes = res
+	})
+	b.Run("yaml.V3", func(b *testing.B) {
+		var res interface{}
+		for i := 0; i < b.N; i++ {
+			var v interface{}
+			_ = yamlV3.Unmarshal(yamlFile, &v)
+			j, _ := yamlJSON(v)
+			res = j
+		}
+		benchmarkRes = res
+	})
+	b.Run("yaml.V2", func(b *testing.B) {
+		var res interface{}
+		for i := 0; i < b.N; i++ {
+			var v interface{}
+			_ = yamlV2.Unmarshal(yamlFile, &v)
+			j, _ := yamlJSON(v)
+			benchmarkRes = j
+		}
+		benchmarkRes = res
+	})
+	b.Run("ghodss", func(b *testing.B) {
+		var res interface{}
+		for i := 0; i < b.N; i++ {
+			var v interface{}
+			_ = ghodss.Unmarshal(yamlFile, &v)
+			benchmarkRes = v
+		}
+		benchmarkRes = res
+	})
+	b.Run("goccy", func(b *testing.B) {
+		var res interface{}
+		for i := 0; i < b.N; i++ {
+			var v interface{}
+			_ = goccy.Unmarshal(yamlFile, &v)
+			benchmarkRes = v
+		}
+		benchmarkRes = res
+	})
+	b.Logf("Res type: %T", benchmarkRes)
+}
